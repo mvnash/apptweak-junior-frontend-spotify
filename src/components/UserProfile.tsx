6f330ed15cada713e4c170235247f7477b7052b3
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-05-21 16:51:59 +0200

    fix: default profile picture if not found

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -7,11 +7,19 @@ interface UserProfileProps {
 const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
   return (
     <div className="UserProfile">
-      <img
-        id="idProfilePicture"
-        src={user?.images[0]?.url}
-        alt="profilePicture"
-      />
+      {user?.images[0] ? (
+        <img
+          id="idProfilePicture"
+          src={user?.images[0]?.url}
+          alt="profilePicture"
+        />
+      ) : (
+        <img
+          id="idProfilePicture"
+          src="Default_pfp.svg"
+          alt="default profile"
+        />
+      )}
       <p>{user ? user.display_name : "Guest"}</p>
     </div>
   );
